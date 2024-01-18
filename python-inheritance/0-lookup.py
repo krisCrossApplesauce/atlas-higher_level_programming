c16b1d4da16e390fbb7ea07390681e6ac283de37
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-18 11:37:06 -0600

    task 0 :)

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Write a func called lookup
+that returns the list of available attributes and methods of an obj
+"""
+
+
+def lookup(obj):
+    """ does stuff """
+    return dir(obj)
