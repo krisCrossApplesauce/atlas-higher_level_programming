14d2776010b3f301042ffedb539e48b54557d702
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-18 11:42:23 -0600

    0: documentation is wrong?..

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-"""
-Write a func called lookup
-that returns the list of available attributes and methods of an obj
-"""
+# Write a func called lookup
+# that returns the list of available attributes and methods of an obj
 
 
 def lookup(obj):
-    """ does stuff """
+    # does stuff
     return dir(obj)
