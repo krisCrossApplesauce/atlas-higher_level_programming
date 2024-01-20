e2235205abede95410883e9e2d9ebe1a541c04d4
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-20 17:05:29 -0600

    task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Square class (inherits from Rectangle which inherits from BaseGeometry """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ it's a square (wow!)"""
+
+    def __init__(self, size):
+        """ initializes the Square """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ area of square obj """
+        return self.__size * self.__size
