3fe9ccb58dce5a22662fab01fa51a39bbfb9ef4b
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-20 15:58:54 -0600

    task 6 :D

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" BaseGeometry, but not empty """
+
+
+class BaseGeometry():
+    """ no longer empty """
+
+    def area(self):
+        """ unimplemented method """
+        raise Exception("area() is not implemented")
