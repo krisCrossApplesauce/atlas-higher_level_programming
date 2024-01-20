6921d69ea4bcafebd8cf5ef32be7af854748005d
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-20 15:52:30 -0600

    4: PEP8

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ it does whatever, I don't feel like writing these """
+
+
 def inherits_from(obj, a_class):
     """ inherits but not exact instance """
     return type(obj) is not a_class and isinstance(obj, a_class)
