553cfa3702a4e7631e43cf28b8d1a81a06eb68ab
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-21 17:15:57 -0600

    task 0 :P

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Write a func that reads a text file and prints to stdout """
+
+
+def read_file(filename=""):
+    """ reads file then prints to stdout """
+    with open(filename, 'r') as file:
+        print(file.read())
