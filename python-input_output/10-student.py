8bff1360b1550511a094e660261ec8442448f998
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-21 20:08:18 -0600

    task 10 :)

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -13,6 +13,12 @@ class Student():
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """ retrieves dict representation of student instance """
+        if attrs is not None:
+            dictionary = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    dictionary[attr] = self.__dict__[attr]
+            return dictionary
         return self.__dict__
