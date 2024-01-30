48bcfe3de52b23ee66bdf4e9c8c62e4d40bd0be6
kris_cross_applesauce <karisrr2203@gmail.com>
2024-01-29 20:42:49 -0600

    task 10 :)

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+"""
+Write a class called Square that inherits from Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ a square class """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ initializes a square instance """
+        super().__init__(size, size, x, y, id)
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size <= 0:
+            raise ValueError("size must be > 0")
+        self.size = size
+
+    def area(self):
+        """ returns the area of the Square instance """
+        return self.__size * self.__size
+
+    def display(self):
+        """ prints in stdout the Square instance using the char # """
+        for _ in range(self.__y):
+            print("")
+        for _ in range(self.__size):
+            for _ in range(self.__x):
+                print(" ", end="")
+            for _ in range(self.__size):
+                print("#", end="")
+            print("")
+
+    def update(self, *args, **kwargs):
+        """ assigns an arg to each attribute """
+        attributes = ["id", "size", "x", "y"]
+        if args is not None and len(args) > 0:
+            for attr, value in zip(attributes, args):
+                setattr(self, attr, value)
+        elif kwargs is not None:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def __str__(self):
+        """ overriding the __str__ method """
+        return f"[Square] ({self.id}) {self.__x}/{self.__y} " \
+            + f"- {self.__size}/{self.__size}"
+
+    @property
+    def size(self):
+        """ size getter """
+        return self.__size
+    
+    @size.setter
+    def size(self, size):
+        """ size setter """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size <= 0:
+            raise ValueError("size must be > 0")
+        self.__size = size
+
+    @property
+    def x(self):
+        """ x getter """
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """ x setter """
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
+
+    @property
+    def y(self):
+        """ y getter """
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """ y setter """
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
---
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" unittest for models/square.py """
+from models.square import Square
+import unittest
+
+
+class testSquare(unittest.TestCase):
+    """ unittest for Square class """
